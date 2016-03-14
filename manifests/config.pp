1d2ae3f53e104e8bfc9a5aa38cedb9ca34d2c45a
Chris H (CruftMaster) <prawn@cruft.co>
2016-03-14 11:42:39 +0000

    Importing config options

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,14 @@
 # == Class: telegraf::config
 #
-class telegraf::config {
+class telegraf::config(
+  $tags                 = $::telegraf::tags,
+  $agent_debug          = $::telegraf::agent_debug,
+  $agent_flush_interval = $::telegraf::agent_flush_interval,
+  $agent_flush_jitter   = $::telegraf::agent_flush_jitter,
+  $agent_hostname       = $::telegraf::agent_hostname,
+  $agent_interval       = $::telegraf::agent_interval,
+  $agent_round_interval = $::telegraf::agent_round_interval,
+) {
 
   # The main Telegraf config file:
   file { $::telegraf::config_file:
