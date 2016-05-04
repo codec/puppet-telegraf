814e636a74b031b664ba32202d350a7134ea932e
Chris H (CruftMaster) <prawn@cruft.co>
2016-05-04 14:22:45 +0100

    Adding support for the NSQ input plugin

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,13 +1,16 @@
 # == Class: telegraf::config
 #
 class telegraf::config(
-  $tags                 = $::telegraf::tags,
-  $agent_debug          = $::telegraf::agent_debug,
-  $agent_flush_interval = $::telegraf::agent_flush_interval,
-  $agent_flush_jitter   = $::telegraf::agent_flush_jitter,
-  $agent_hostname       = $::telegraf::agent_hostname,
-  $agent_interval       = $::telegraf::agent_interval,
-  $agent_round_interval = $::telegraf::agent_round_interval,
+  $tags                         = $::telegraf::tags,
+  $agent_collection_jitter      = $::telegraf::agent_collection_jitter,
+  $agent_debug                  = $::telegraf::agent_debug,
+  $agent_flush_buffer_when_full = $::telegraf::agent_flush_buffer_when_full,
+  $agent_flush_interval         = $::telegraf::agent_flush_interval,
+  $agent_flush_jitter           = $::telegraf::agent_flush_jitter,
+  $agent_hostname               = $::telegraf::agent_hostname,
+  $agent_interval               = $::telegraf::agent_interval,
+  $agent_metric_buffer_limit    = $::telegraf::agent_metric_buffer_limit,
+  $agent_round_interval         = $::telegraf::agent_round_interval,
 ) {
 
   # The main Telegraf config file:
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,22 @@
 # Manage InfluxDBs Telegraf monitoring agent:
 #
 class telegraf (
-  $ensure                    = 'installed',
-  $version                   = '0.10.2-1',
-  $install_from_repository   = false,
-  $config_file               = '/etc/telegraf/telegraf.conf',
-  $config_directory          = '/etc/telegraf/telegraf.d',
-  $tags                      = undef,
-  $agent_debug               = false,
-  $agent_flush_interval      = '10s',
-  $agent_flush_jitter        = '0s',
-  $agent_hostname            = $::hostname,
-  $agent_interval            = '10s',
-  $agent_round_interval      = true,
+  $ensure                       = 'installed',
+  $version                      = '0.12.1-1',
+  $install_from_repository      = false,
+  $config_file                  = '/etc/telegraf/telegraf.conf',
+  $config_directory             = '/etc/telegraf/telegraf.d',
+  $tags                         = undef,
+  $agent_collection_jitter      = '0s',
+  $agent_debug                  = false,
+  $agent_flush_buffer_when_full = true,
+  $agent_flush_interval         = '10s',
+  $agent_flush_jitter           = '0s',
+  $agent_hostname               = $::hostname,
+  $agent_interval               = '10s',
+  $agent_metric_buffer_limit    = 1000,
+  $agent_round_interval         = true,
+
 ) {
 
   class { '::telegraf::package': } ~>
